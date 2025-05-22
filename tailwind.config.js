30e7a98faddab1a10a547aa63f53470087b25efb
Sahibealam2003 <alamsahib9719@gmail.com>
2025-05-22 06:46:43 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
+    content: ["./index.html"],
     theme: {
-      extend: {},
+      extend: {
+        
+      },
     },
     plugins: [],
   }
